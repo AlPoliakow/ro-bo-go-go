fbe6d079aee595018fff8404466d5f6ab96577bc
Al_P <al.e.poliakow@gmail.com>
2024-08-19 22:34:50 +1000

    Corrected grid numbering

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ let position = {x: 0, y: 0};
 
 
 function makeGrid(rows, cols) {
-  
-  
   let x = 0;
   let y = 0;
-  for (let c = 0; c < rows * cols; c++) {
+  for (let c = 0; c < rows; c++) {
+    for (let d = 1; d <= cols; d++) {
     let cell = document.createElement("div");
     
     y = c%cols;
@@ -22,12 +21,20 @@ function makeGrid(rows, cols) {
     if (y === (rows - 1)) {
     	x++;
     }    
+
+   // let position = { x: 0, y: 1 }; for (let c = 0; c < rows; c++) { for (let d = 1; d <= cols; d++) {
+        //let cell = document.createElement("div"); 
+        container.appendChild(cell).className = "grid-item grid-item-" + c + "" + d;
+   };
         
-    container.appendChild(cell).className = "grid-item grid-item-" + x + '' + y;
-  }
+    //container.appendChild(cell).className = "grid-item grid-item-" + x + '' + y;
+    //container.appendChild(cell).className = `"grid-item ("grid-item-" + x) + (".grid-item-" + y)` ;
+   // container.appendChild(cell).className = "grid-item grid-item-" + x + '' + y + c;
+  //}
 
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
+}
 }
 const keys = {
   left: 37,
@@ -39,9 +46,9 @@ const keys = {
 makeGrid(5, 5); // 5x5 grid
 circle.style.width = "80px";
 circle.style.height = "80px";
-circle.style.margin = "10px";
+circle.style.margin = "10px"; // center the div
 circle.style.background = "#FFFFFF";
-circle.innerHTML = `<i class="fa-solid fa-robot"></i>`;
+circle.innerHTML = `<i class="fa-solid fa-robot robot"></i>`;
 let firstGridItem = document.querySelector(".grid-item");
 firstGridItem.appendChild(circle);
 
@@ -49,7 +56,7 @@ let squares = document.querySelectorAll(".grid-item");
 console.log(squares);
 
 
-let startSquare = document.querySelector(".grid-item-22");
+let startSquare = document.querySelector(".grid-item-23");
 console.log(startSquare);
 
 //startSquare.append(circle);
