7eb60ca6f6e6ed5b3f9b1ec78645bde0449fea0b
Al_P <al.e.poliakow@gmail.com>
2024-08-25 00:05:57 +1000

    Created grid with robot in center and rotation capabilities

--- app.js
+++ app.js
@@ -1,86 +1,94 @@
 const gameBoard = document.querySelector("#gameboard");
-const width = 5;
+const controls = document.querySelector("#controls");
+const gridBot = document.createElementNS("http://www.w3.org/2000/svg", `svg`);
 
+for (let i = 1; i < 26; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridItem.classList.add("space");
+    gridItem.textContent = i;
+    gameBoard.appendChild(gridItem);
 
-const container = document.querySelector(".grid");
-const gridNodes = document.querySelectorAll(".grid-item");
-const gridArray = Array.from(gridNodes);
-let circle = document.createElement("div");
-let position = {x: 0, y: 0};
-
-
-function makeGrid(rows, cols) {
-  let x = 0;
-  let y = 0;
-  for (let c = 0; c < rows; c++) {
-    for (let d = 1; d <= cols; d++) {
-    let cell = document.createElement("div");
-    
-    y = c%cols;
-    
-    if (y === (rows - 1)) {
-    	x++;
-    }    
-
-   // let position = { x: 0, y: 1 }; for (let c = 0; c < rows; c++) { for (let d = 1; d <= cols; d++) {
-        //let cell = document.createElement("div"); 
-        container.appendChild(cell).className = "grid-item grid-item-" + c + "" + d;
-   };
+    //square 13 appendChild image
+    if (i == 13) {
+        gridItem.innerHTML = "";
         
-    //container.appendChild(cell).className = "grid-item grid-item-" + x + '' + y;
-    //container.appendChild(cell).className = `"grid-item ("grid-item-" + x) + (".grid-item-" + y)` ;
-   // container.appendChild(cell).className = "grid-item grid-item-" + x + '' + y + c;
-  //}
+        const iconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
-  container.style.setProperty("--grid-rows", rows);
-  container.style.setProperty("--grid-cols", cols);
+        gridBot.setAttribute('fill', 'black'); //colours it in
+        gridBot.setAttribute('viewBox', '0 0 640 512'); //from svg link
+        gridBot.setAttribute('stroke', 'black'); // color
+
+        iconPath.setAttribute("d", "M320 0c17.7 0 32 14.3 32 32l0 64 120 0c39.8 0 72 32.2 72 72l0 272c0 39.8-32.2 72-72 72l-304 0c-39.8 0-72-32.2-72-72l0-272c0-39.8 32.2-72 72-72l120 0 0-64c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224l16 0 0 192-16 0c-26.5 0-48-21.5-48-48l0-96c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-16 0 0-192 16 0z");
+        //iconPath.setAttribute('stroke-linecap', 'round');
+        //iconPath.setAttribute('stroke-linejoin', 'round');
+        iconPath.setAttribute('stroke-width', '6'); //thickness of lines
+        gridBot.appendChild(iconPath);
+        gridBot.classList.add("robot");
+
+
+        gridItem.appendChild(gridBot);
+        console.log(gridItem);
+    } 
+    
 }
-}
-const keys = {
-  left: 37,
-  up: 38,
-  right: 39,
-  down: 40
-};
 
-makeGrid(5, 5); // 5x5 grid
-circle.style.width = "80px";
-circle.style.height = "80px";
-circle.style.margin = "10px"; // center the div
-circle.style.background = "#FFFFFF";
-circle.innerHTML = `<i class="fa-solid fa-robot robot"></i>`;
-let firstGridItem = document.querySelector(".grid-item");
-firstGridItem.appendChild(circle);
+const rotateLeft = document.createElement("button");
+rotateLeft.innerText = "Rotate left";
+controls.appendChild(rotateLeft);
 
-let squares = document.querySelectorAll(".grid-item");
-console.log(squares);
+rotateLeft.addEventListener("click",  function(e){
+    e.preventDefault(); 
+    gridBot.setAttribute("transform", `rotate(90)`);
+})
 
+const rotateRight = document.createElement("button");
+rotateRight.innerText = "Rotate right";
+controls.appendChild(rotateRight);
 
-let startSquare = document.querySelector(".grid-item-23");
-console.log(startSquare);
+rotateRight.addEventListener("click",  function(e){
+    e.preventDefault(); 
+    gridBot.setAttribute("transform", `rotate(-90)`);
+})
 
-//startSquare.append(circle);
+const rotateUp = document.createElement("button");
+rotateUp.innerText = "Rotate up";
+controls.appendChild(rotateUp);
 
-function handleKey(e) {
-  switch (e.keyCode) {
-    case keys.left:
-      position.y--;
-      break;
-    case keys.up:
-      position.x--;
-      break;
+rotateUp.addEventListener("click",  function(e){
+    e.preventDefault(); 
+    gridBot.setAttribute("transform", `rotate(180)`);
+})
 
-    case keys.right:
-      position.y++;
-      break;
+const rotateDown = document.createElement("button");
+rotateDown.innerText = "Rotate down";
+controls.appendChild(rotateDown);
 
-    case keys.down:
-      position.x++;
-      break;
-  }
-  
-  let gridItem = document.querySelector(".grid-item-" + position.x + '' + position.y);
-  gridItem.appendChild(circle);
-}
-window.addEventListener("keydown", handleKey);
+rotateDown.addEventListener("click",  function(e){
+    e.preventDefault(); 
+    gridBot.setAttribute("transform", `rotate(0)`);
+})
 
+const advance = document.createElement("button");
+const robotRotation = gridBot.style.transform;
+advance.innerText = "Advance";
+controls.appendChild(advance);
+
+advance.addEventListener("click",  function(e){
+    e.preventDefault(); 
+    console.log("clicked");
+    switch (gridBot.getAttribute(transform)){ //error here
+    case "rotate(0)":
+        console.log("Rotation 0");
+        break;
+    case "rotate(90)":
+        console.log("Rotation 90");
+        break;
+    case "rotate(-90)":
+        console.log("Rotation -90");
+        break;
+    case "rotate(180)":
+        console.log("Rotation 180");
+        break;
+    }
+})
