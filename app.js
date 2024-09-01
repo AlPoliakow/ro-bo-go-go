e21cfd769b80bea281961e7c162e7cdbdb461be8
Al_P <al.e.poliakow@gmail.com>
2024-09-01 23:11:30 +1000

    Updated instructions styles

--- app.js
+++ app.js
@@ -23,9 +23,6 @@ controls.appendChild(turns);
 const gameBoard = document.createElement("div");
 gameBoard.classList.add("gameboard");
 app.appendChild(gameBoard);
-//document.querySelector("#gameboard");
-//const controls = document.querySelector("#controls");
-//const turns = document.querySelector("#turns");
 
 //create written by section
 const by = document.createElement("p");
@@ -36,16 +33,8 @@ by.style.width="300px";
 
 const gridBot = document.createElementNS("http://www.w3.org/2000/svg", `svg`);
 const robotInfo = getComputedStyle(gridBot);
-//const instructions = document.querySelector(".instructions");
-//const by = document.querySelector(".by");
 
-//console.log(robotInfo);
-//const robotTransform = robotInfo[314]; // 254 for rotate but still doesn't work
-//console.log(robotTransform); // =transform ????
 const robotTransform = robotInfo.getPropertyValue("transform");
-
-//instructions.innerText="Click the buttons to turn and move the robot";
-//by.innerHTML=`<p>By Al Poliakow 2024</p>`;
 
 // create a 5x5 gameboard
 for (let i = 1; i < 26; i++) {
@@ -62,14 +51,12 @@ for (let i = 1; i < 26; i++) {
     if (i == 13) {
         const iconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
-        gridBot.setAttribute('fill', '#e15a1d'); //colours it in
+        gridBot.setAttribute('fill', '#5c5c5c'); //colours it in
         gridBot.setAttribute('viewBox', '0 0 640 512'); //from svg link
-        gridBot.setAttribute('stroke', '#e15a1d'); // color
+        gridBot.setAttribute('stroke', '#5c5c5c'); // color
         gridBot.setAttribute("transform", `rotate(0)`); // to make advancing work before button pressing
 
         iconPath.setAttribute("d", "M320 0c17.7 0 32 14.3 32 32l0 64 120 0c39.8 0 72 32.2 72 72l0 272c0 39.8-32.2 72-72 72l-304 0c-39.8 0-72-32.2-72-72l0-272c0-39.8 32.2-72 72-72l120 0 0-64c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224l16 0 0 192-16 0c-26.5 0-48-21.5-48-48l0-96c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-16 0 0-192 16 0z");
-        //iconPath.setAttribute('stroke-linecap', 'round');
-        //iconPath.setAttribute('stroke-linejoin', 'round');
         iconPath.setAttribute('stroke-width', '6'); //thickness of lines
         gridBot.appendChild(iconPath);
         gridBot.classList.add("robot");
@@ -85,7 +72,7 @@ const divs = document.querySelectorAll(".space");
 
 // create a button to rotate the robot upwards
 const rotateUp = document.createElement("button");
-rotateUp.innerText = "Turn up";
+rotateUp.innerText = "Face up";
 rotateUp.style.margin="10px 40px";
 turns.appendChild(rotateUp);
 
@@ -99,7 +86,7 @@ rotateUp.addEventListener("click", function (e) {
 
 // create a button to rotate the robot left
 const rotateLeft = document.createElement("button");
-rotateLeft.innerText = "Turn left";
+rotateLeft.innerText = "Face left";
 rotateLeft.style.margin="0 5px";
 turns.appendChild(rotateLeft);
 
@@ -112,7 +99,7 @@ rotateLeft.addEventListener("click", function (e) {
 
 // create a button to rotate the robot right
 const rotateRight = document.createElement("button");
-rotateRight.innerText = "Turn right";
+rotateRight.innerText = "Face right";
 rotateRight.style.margin="0 5px";
 turns.appendChild(rotateRight);
 
@@ -125,7 +112,7 @@ rotateRight.addEventListener("click", function (e) {
 
 // create a button to rotate the robot downwards
 const rotateDown = document.createElement("button");
-rotateDown.innerText = "Turn down";
+rotateDown.innerText = "Face down";
 rotateDown.style.margin="10px 40px";
 turns.appendChild(rotateDown);
 
@@ -138,8 +125,8 @@ rotateDown.addEventListener("click", function (e) {
 
 const advance = document.createElement("button");
 advance.style.margin="25px 30px";
-advance.style.width="50px";
-advance.innerText = "Go";
+advance.style.width="60px";
+advance.innerText = "Move";
 controls.appendChild(advance);
 
 advance.addEventListener("click", function (e) {
@@ -173,9 +160,6 @@ advance.addEventListener("click", function (e) {
 
 
                 switch (parent) {  // processing the parent divs index
-                    //case '13':
-                    // console.log("Registered the switch"); //registered
-                    // break;
                     //make it move
                     case '1':
                     case '2':
@@ -210,7 +194,7 @@ advance.addEventListener("click", function (e) {
                     case '24':
                     case '25':
                         console.log("do not advance"); //registered
-                        instructions.innerText = "You can't sink any lower";
+                        instructions.innerText = "Oops. You can't go any lower.";
                         break;
                 }
             })
@@ -264,7 +248,7 @@ advance.addEventListener("click", function (e) {
                     case '16':
                     case '21':
                         console.log("do not advance"); //registered
-                        instructions.innerText = "There's nowhere *left* to go";
+                        instructions.innerText = "Oops. There's nowhere *left* to go.";
                         break;
                 }
             })
@@ -318,7 +302,7 @@ advance.addEventListener("click", function (e) {
                     case '20':
                     case '25':
                         console.log("do not advance"); //registered
-                        instructions.innerText = "Two rights is not making a left";
+                        instructions.innerText = "Oops. Two rights is not making a left.";
                         break;
                 }
             })
@@ -372,7 +356,7 @@ advance.addEventListener("click", function (e) {
                     case '4':
                     case '5':
                         console.log("do not advance"); //registered
-                        instructions.innerText = "You've reached the ceiling";
+                        instructions.innerText = "Oops. You've already reached the top.";
                         break;
                 }
             })
